a216667828b439a34deec84209fb3c5a0af40642
Chak <444chak@gmail.com>
2024-09-27 11:15:30 +0200

    ✨feature: UneFenetre : ajouter le mobile à la fenêtre et lancer un thread pour l'animation

--- TP1_Mobile_src/UneFenetre.java
+++ TP1_Mobile_src/UneFenetre.java
@@ -1,17 +1,17 @@
 import java.awt.*;
 import javax.swing.*;
 
-class UneFenetre extends JFrame 
-{
+class UneFenetre extends JFrame {
     UnMobile sonMobile;
-    private final int LARG=400, HAUT=250;
-    
-    public UneFenetre()
-    {
-	// TODO 
-	// ajouter sonMobile a la fenetre
-	// creer une thread laThread avec sonMobile
-	// afficher la fenetre
-	// lancer laThread 
+    private final int LARG = 400, HAUT = 250;
+
+    public UneFenetre() {
+        sonMobile = new UnMobile(LARG, HAUT);
+        add(sonMobile); // ajouter sonMobile a la fenetre
+        Thread laThread = new Thread(sonMobile); // creer une thread laThread avec sonMobile
+        setSize(LARG, HAUT); // définir la taille de la fenêtre
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // définir l'opération de fermeture par défaut
+        setVisible(true);// afficher la fenetre
+        laThread.start(); // lancer laThread
     }
 }
