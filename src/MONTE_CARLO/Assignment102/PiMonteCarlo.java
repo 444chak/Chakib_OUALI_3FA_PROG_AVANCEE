da3792b75e75bb18831f2034e9910148f418a0d9
Chak <444chak@gmail.com>
2025-01-27 17:59:21 +0100

    :art: Ajouter la méthode getResult() pour encapsuler les résultats de la simulation Monte Carlo

--- src/MONTE_CARLO/Assignment102/PiMonteCarlo.java
+++ src/MONTE_CARLO/Assignment102/PiMonteCarlo.java
@@ -1,11 +1,10 @@
 package MONTE_CARLO.Assignment102;
 // Estimate the value of Pi using Monte-Carlo Method, using parallel program
 
+import MONTE_CARLO.Result;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-
-
 
 public class PiMonteCarlo {
 
@@ -32,5 +31,14 @@ public class PiMonteCarlo {
         }
         value = 4.0 * nAtomSuccess.get() / nThrows;
         return value;
+    }
+
+    public Result getResult() {
+        double error = (Math.abs((value - Math.PI)) / Math.PI);
+        int ntot = nThrows;
+        int nbProcess = Runtime.getRuntime().availableProcessors();
+        long time = 0;
+        Result result = new Result(nAtomSuccess.get(), error, value, ntot, nbProcess, time);
+        return result;
     }
 }
