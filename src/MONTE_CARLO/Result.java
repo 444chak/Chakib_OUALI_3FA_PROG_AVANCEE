d47daf0d6d1789e8e10e9f9ff2c757b0ea1d2759
Chak <444chak@gmail.com>
2025-01-27 17:59:11 +0100

    :art: Ajouter la classe Result pour encapsuler les résultats de la simulation Monte Carlo

---
+++ src/MONTE_CARLO/Result.java
@@ -0,0 +1,48 @@
+package MONTE_CARLO;
+
+public class Result {
+
+    private final long total;
+    private final double error;
+    private final double estimation;
+    private final int ntot;
+    private final int nbProcess;
+    private long time;
+
+    public Result(long total, double error, double estimation, int ntot, int nbProcess, long time) {
+        this.total = total;
+        this.error = error;
+        this.estimation = estimation;
+        this.ntot = ntot;
+        this.nbProcess = nbProcess;
+        this.time = time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public double getEstimation() {
+        return estimation;
+    }
+
+    public int getNtot() {
+        return ntot;
+    }
+
+    public int getNbProcess() {
+        return nbProcess;
+    }
+
+    public long getTime() {
+        return time;
+    }
+}
