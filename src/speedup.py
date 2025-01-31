15f410659f84c5ec7a7daa0822d8da172f3cf86a
Chak <444chak@gmail.com>
2025-01-31 11:26:09 +0100

    :art: Refactor speedup.py pour intégrer des arguments de ligne de commande et améliorer la scalabilité des simulations Monte Carlo

--- src/speedup.py
+++ src/speedup.py
@@ -5,15 +5,15 @@ import csv
 
 
 import pathlib
+import argparse
 
 path = pathlib.Path(__file__).parent.absolute()
 path = path / "MONTE_CARLO"
 
-NUMBER_OF_EXPERIMENTS = 10
-TOTAL_COUNT = 1200
+dir_out = pathlib.Path(__file__).parent.absolute() / "MONTE_CARLO" / "out"
 
 
-def run_java(workers):
+def run_java(workers, number_of_experiments, total_count):
     file = path / "Main.java"
     subprocess.run(["javac", file])
     result = subprocess.run(
@@ -21,59 +21,91 @@ def run_java(workers):
             "java",
             "MONTE_CARLO.Main",
             str(workers),
-            str(NUMBER_OF_EXPERIMENTS),
-            str(TOTAL_COUNT),
+            str(number_of_experiments),
+            str(total_count),
         ],
         stdout=subprocess.PIPE,
     )
     return result.stdout.decode("utf-8")
 
 
-speedup_times = []
-
 nb_workers = [1, 2, 3, 4, 5, 6, 8, 10, 12, 16, 18]
 
-for i in nb_workers:
-    out = run_java(i)
-    print("-----------------")
-    print(out.strip().split("\n")[-1])
 
-    with open(
-        pathlib.Path(__file__).parent.absolute() / out.strip().split("\n")[-1]
-    ) as f:
-        data = csv.reader(f, delimiter="\t")
-        header = next(data)  # Skip the header
-        data = list(data)
+def scalabilite_forte(nb_workers, nb_experiments=10, total_count=1200000):
+    speedup_times = []
+    for i in nb_workers:
+        print(
+            "Running Master-Worker with ", i, " workers", "| Total count: ", total_count
+        )
+        out = run_java(i, nb_experiments, total_count)
 
-    times = [float(row[header.index("Time")]) for row in data]
+        print(out.strip().split("\n")[-1])
 
-    speedup_times.append(np.mean(times))
+        with open(
+            pathlib.Path(__file__).parent.absolute() / out.strip().split("\n")[-1]
+        ) as f:
+            data = csv.reader(f, delimiter="\t")
+            header = next(data)  # Skip the header
+            data = list(data)
 
-    print("Mean time (ms): ", np.mean(times), "| with workers: ", i)
+        times = [float(row[header.index("Time")]) for row in data]
+
+        speedup_times.append(np.mean(times))
+
+        print("Mean time (ms): ", np.mean(times))
+        print("-----------------")
+
+    # clear the directory
+    for f in dir_out.iterdir():
+        f.unlink()
+
+    return speedup_times
 
 
-sP = list(map(lambda x: speedup_times[0] / x, speedup_times))
+def plot_scalabilite_forte(nb_workers, speedup_times, speedup2=None, speedup3=None):
+    sP = list(map(lambda x: speedup_times[0] / x, speedup_times))
+    sP2 = list(map(lambda x: speedup2[0] / x, speedup2)) if speedup2 else None
+    sP3 = list(map(lambda x: speedup3[0] / x, speedup3)) if speedup3 else None
 
-plt.plot(nb_workers, sP, label="Speedup", marker="o")
-plt.xlabel("Number of workers")
-plt.ylabel("Speedup")
-plt.title("Speedup vs Number of workers")
-plt.grid()
-# perfect speedup
-plt.plot([1, nb_workers[-1]], [1, nb_workers[-1]], "--", label="Perfect Speedup")
-plt.legend()
-plt.axis("equal")
-max_value = max(nb_workers)
-plt.xlim(1, max_value)
-plt.ylim(1, max_value)
-plt.yticks(range(1, max_value + 1))
-plt.xticks(range(1, max_value + 1))
-# adjust the size of the plot
-plt.gcf().set_size_inches(10, 7)
-plt.show()
+    plt.plot(nb_workers, sP, label="Speedup", marker="o")
+    if speedup2:
+        plt.plot(nb_workers, sP2, label="Speedup (2)", marker="o")
+    if speedup3:
+        plt.plot(nb_workers, sP3, label="Speedup (3)", marker="o")
+    plt.xlabel("Number of workers")
+    plt.ylabel("Speedup")
+    plt.title("Speedup vs Number of workers")
+    plt.grid()
+    # perfect speedup
+    plt.plot([1, nb_workers[-1]], [1, nb_workers[-1]], "--", label="Perfect Speedup")
+    plt.legend()
+    plt.axis("equal")
+    max_value = max(nb_workers)
+    plt.xlim(1, max_value)
+    plt.ylim(1, max_value)
+    plt.yticks(range(1, max_value + 1))
+    plt.xticks(range(1, max_value + 1))
+    # adjust the size of the plot
+    plt.gcf().set_size_inches(10, 7)
+    plt.show()
 
-dir_out = pathlib.Path(__file__).parent.absolute() / "MONTE_CARLO" / "out"
 
-# clear the directory
-for f in dir_out.iterdir():
-    f.unlink()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run Monte Carlo simulation with multiple workers"
+    )
+    parser.add_argument(
+        "--experiments", type=int, default=10, help="Number of experiments"
+    )
+    parser.add_argument("--count", type=int, default=1200000000, help="Total count")
+
+    args = parser.parse_args()
+    speedup_times = scalabilite_forte(nb_workers, args.experiments, args.count)
+    speedup_times2 = scalabilite_forte(nb_workers, args.experiments, args.count // 100)
+    speedup_times3 = scalabilite_forte(nb_workers, args.experiments, args.count // 1000)
+    plot_scalabilite_forte(nb_workers, speedup_times, speedup_times2, speedup_times3)
+
+
+if __name__ == "__main__":
+    main()
